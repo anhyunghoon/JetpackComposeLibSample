d82ee8bb0c6a3d9f44eff811d138f12224f63201
hhan1993 <>
2025-05-20 16:48:55 +0900

    Revert "커밋1"
    This reverts commit 4b317c36365410d3a0f3631236f9c59eca6d54f7.

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    // test 8
+    // test 7
 
     Text(
         text = "Hello $name!", modifier = modifier
